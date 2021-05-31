fa231887881ac81bd1ac511f8b1007be543d998d
lioonya17 <57496522+lioonya17@users.noreply.github.com>
2021-05-31 14:37:36 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".item").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: true
+    },
+    removalDelay: 300,
+    mainClass: 'mfp-fade'
+  })
+})
