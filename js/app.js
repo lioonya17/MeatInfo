bd82999f2f1599070c5c9cbd08842374ad9d2c43
lioonya17 <57496522+lioonya17@users.noreply.github.com>
2021-06-01 11:13:02 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
-  $(".item").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true
-    },
-    removalDelay: 300,
-    mainClass: 'mfp-fade'
-  })
-})
+const menuButton = document.querySelector(".menu-button")
+const menu = document.querySelector(".navbar")
+const closeMenuButton = document.querySelector(".close-btn")
+
+const toggleMenu = (event) => {
+  menu.classList.toggle("is-open")
+}
+
+menuButton.addEventListener("click", toggleMenu)
+closeMenuButton.addEventListener("click", toggleMenu)
+
